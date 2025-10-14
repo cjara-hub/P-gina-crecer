f4a85265fbfff507c393589602d11a1928225491
cjara-hub <cjara@consultoracrecer.cl>
2025-10-14 16:52:57 -0300

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import Image from 'next/image'; // Importa el componente Image
 
 const Logo: React.FC<{ className?: string }> = ({ className = 'h-24' }) => {
   return (
     <div className={`flex items-center ${className}`}>
       <a href="#inicio" aria-label="Volver al inicio">
-        <Image 
+        <img 
           src="/assets/logo.png" 
           alt="Logo de Fundación Crecer Consultora Educacional"
-          width={150} // Debes especificar un ancho (ejemplo)
-          height={60} // Debes especificar un alto (ejemplo)
-          className="h-full w-auto" // Puedes mantener tus clases para responsive
+          className="h-full w-auto"
         />
       </a>
     </div>
